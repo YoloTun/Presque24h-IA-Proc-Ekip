20e14b091ef90094a5631fabe448f312e27689b0
LeoPisan <webtools.trough480@passinbox.com>
2025-05-02 13:38:38 +0200

    Ajout d'éléments aux messages pour les rendre plus fonctionnels

--- Interface-communication/Message.cs
+++ Interface-communication/Message.cs
@@ -7,10 +7,25 @@ namespace Interface_communication;
 /// </summary>
 public class Message
 {
-    private string messageServeur;
-    private List<string> arguments;
+    private readonly string messageServeur;
+    private readonly List<string> arguments;
+
+    public Message(string messageServeur)
+    {
+        this.messageServeur = messageServeur;
+        arguments = [];
+    }
 
     private string PrintableArguments => string.Join(Config.ArgumentsDelimiter, arguments);
     
     public string MessageServeur => $"{messageServeur}{Config.ArgumentsDelimiter}{PrintableArguments}";
+
+    /// <summary>
+    /// Ajoute un nouvel argument au message
+    /// </summary>
+    /// <param name="argument">The argument to be added to the list.</param>
+    public void AddArgument(string argument)
+    {
+        arguments.Add(argument);
+    }
 }
