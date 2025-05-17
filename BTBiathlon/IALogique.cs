f07d825026e66f4d46baef358b39a4f929f4cefe
VIALAMorgane <morgane.viala@utt.fr>
2025-05-17 16:42:33 +0200

    feat: Ajout de la DrunkenIA

---
+++ BTBiathlon/IALogique.cs
@@ -0,0 +1,29 @@
+﻿using Interface_communication;
+
+namespace BTBiathlon;
+
+public class IALogique : IntelligenceArtificielle
+{
+    public override List<Message> GetProtocoleDemarragePartie()
+    {
+        return [new Message("Bêta-Testeurs2")];
+    }
+
+    public override List<Message> PhaseTour(int tour, int phase, List<ReponseServeur> reponsesServeur)
+    {
+        List<Message> messages = new List<Message>();
+        if (phase != 17) //Pas nuit de sang
+        {
+            if (phase % 4 == 0) //NUIT 
+            {
+                messages.Add(new Message(Dictionnaire.Utiliser, ["SAVOIR"]));
+                messages.Add(new Message(Dictionnaire.Attaquer, ["0"]));
+            }
+            else //Jour
+            {
+                messages.Add(new Message(Dictionnaire.Piocher, ["2"]));
+            }
+        }
+        return messages;
+    }
+}
--- BTBiathlon/Program.cs
+++ BTBiathlon/Program.cs
@@ -1,3 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using BTBiathlon;
+using Interface_communication.Utils;
+using Interface_communication.Utils.Logging;
+
+//Config
+Config.NombrePhaseTour= 17;//Potentiellement faire une énum ?
+Logger.NiveauLog = NiveauxLog.Info;
+
+//IA
+IALogique drunken = new IALogique();
+drunken.Jouer();
