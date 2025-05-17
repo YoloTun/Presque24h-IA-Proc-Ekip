ddbac5d13c711ced7af38c4a8bdfd06ad810bcc3
LeoPisan <webtools.trough480@passinbox.com>
2025-05-17 17:12:07 +0200

    refactor(Interface-Communication): changement de scope pour le getter de VerbeMessage

--- Interface-communication/Message.cs
+++ Interface-communication/Message.cs
@@ -28,7 +28,7 @@ public class Message
 
     private string PrintableArguments => string.Join(Config.ArgumentsDelimiter, arguments);
 
-    protected string VerbeMessage => verbeMessage;
+    public string VerbeMessage => verbeMessage;
     protected List<string> Arguments => arguments;
     
     /// <summary>
